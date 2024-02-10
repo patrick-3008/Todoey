bceb85b07c5c3679746cd45e8c64e5cc259211d5
Patrick <patricknaashat@yahoo.com>
2024-02-10 13:48:14 +0200

    add tableview list to screen

--- Todoey/TodoListVC.swift
+++ Todoey/TodoListVC.swift
@@ -1,10 +1,23 @@
 import UIKit
 
 class TodoListVC: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon", "4"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    //MARK - Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
 }
