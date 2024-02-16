794c41b5b589cc23871ae6e5d652a263fe09d300
Patrick <patricknaashat@yahoo.com>
2024-02-16 15:34:44 +0200

    create category screen with VC

---
+++ Todoey/Controllers/CategoryVC.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class CategoryVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+}
